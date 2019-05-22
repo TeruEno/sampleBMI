2496e2b2e05dc21e47e95b801c69cd6c4e978df3
榎本旭晃 <kingdomhearts.sora319@gmail.com>
2019-05-22 16:07:18 +0800

    Initial Commit

---
+++ sampleBMI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sampleBMI
+//
+//  Created by TeruakiEnomoto on 22/05/2019.
+//  Copyright © 2019 TeruakiEnomoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+     override func viewDidLoad() {
+          super.viewDidLoad()
+          // Do any additional setup after loading the view.
+     }
+
+
+}
+
