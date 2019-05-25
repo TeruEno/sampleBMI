944aa2ddaeb605f23c451b5babddb8a6f4f7896f
榎本旭晃 <kingdomhearts.sora319@gmail.com>
2019-05-25 10:51:24 +0800

    機能追加
    BMIと適正体重を計算する機能を追加するため

--- sampleBMI/ViewController.swift
+++ sampleBMI/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
      @IBOutlet weak var bmiLabel: UILabel!
      @IBOutlet weak var weightTextField: UITextField!
      @IBOutlet weak var heightTextField: UITextField!
+     @IBOutlet weak var weightMatchLabel: UILabel!
+     
      
      
      override func viewDidLoad() {
@@ -22,10 +24,19 @@ class ViewController: UIViewController {
      }
 
      @IBOutlet weak var bmiResultLabel: UILabel!
+     @IBOutlet weak var weightMatchValue: UILabel!
      
      @IBAction func didClickBtn(_ sender: UIButton) {
-//          計算結果を代入
-//          bmiResultLabel.text =
+//          テキストフィールドの値をDouble型に変換
+          let wvalue: Double? = Double(weightTextField.text!)
+          let hvalue: Double? = Double(heightTextField.text!)
+//          hvalueの値の単位をcmをmに合わせる
+          let hchange = hvalue! / 100.0
+          //          計算結果を代入
+//          BMIの計算
+          bmiResultLabel.text =  "\(wvalue! / (hchange * hchange))"
+//          適正体重の計算
+          weightMatchValue.text = "\((hvalue! * hchange) * 22.0 / 100)"
      }
      
 }
